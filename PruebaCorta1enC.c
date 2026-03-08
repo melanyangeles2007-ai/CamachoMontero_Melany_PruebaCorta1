8eb57a03875bca2a49126f0d92372fafd34779c2
melanyangeles2007-ai <melanyangeles2007@gmail.com>
2026-03-08 01:25:09 +0000

    Prueba Corta 1 Grupo 4 Programacion

---
+++ PruebaCorta1enC.c
@@ -0,0 +1,17 @@
+/* Nombre Estudiante: Melany Camacho Montero */
+#include <stdio.h>
+
+int main() {
+        long lng_Indentificacion;
+        char c_NombreCompleto[] = "Camacho Montero Melany";
+        printf("Ingresar su nummero de identificacio: ");
+        scanf("%ld", &lng_Indentificacion);
+
+        printf("Identificacio: %ld\n", lng_Indentificacion);
+        printf("Nombre Completo: %s\n", c_NombreCompleto);
+        
+        return 0;
+        
+}
+
+/* Nombre: Melany Camacho Montero */
---
+++ PruebaCorta1enPython.py
@@ -0,0 +1,12 @@
+# Nombre Estudiante: Melany Camacho Montero
+
+i_Identificacion = int(input("Ingrese el número de indentificación: "))
+
+#Variable con el Nombre Completo
+
+str_NombreCompleto = "Melany Camacho Montero"
+
+print("Identificación: ", i_Identificacion)
+print("Nombre Completo: ", str_NombreCompleto)
+
+# Nombre Estudiante: Melany Camacho Montero
